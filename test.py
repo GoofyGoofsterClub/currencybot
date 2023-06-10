003746638fbb6a0d5dd323071a5185c48d9d7339
mishashto <me@reze.moe>
2023-06-11 07:19:10 +0900

    you dont belong here

--- test.py
+++
@@ -1,8 +0,0 @@
-message = "so $450 message long"
-word = '$450'
-# get just the number
-amount = message.split()[message.split().index(word) - 1]
-amount = ''.join([i for i in amount if i.isnumeric()])
-print(amount)
-amount = float(amount)
-print(amount)
