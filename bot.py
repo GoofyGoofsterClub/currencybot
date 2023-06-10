4d3f97172cc1d272b9d3101c66e9d686f3c81afb
Mikhail <jackglowpetux@gmail.com>
2023-06-11 05:46:33 +0900

    Completely remove the mention of dotenv

--- bot.py
+++ bot.py
@@ -1,11 +1,9 @@
 import discord
 import os
 import json
-#from dotenv import load_dotenv
 import string
 from easy_exchange_rates import API
 from datetime import datetime, timedelta
-#load_dotenv()
 
 currencyapi = api = API()
 
