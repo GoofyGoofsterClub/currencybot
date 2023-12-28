502bed73c00d45fed58d219d8ecbf6f5771e0265
Mikhail <me@reze.moe>
2023-12-28 19:26:23 +0900

    rewrite goofy code

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import discord
 import os
+import requests
 import json
 import re
 from datetime import datetime, timedelta
@@ -31,6 +32,24 @@ def does_text_contain_currency(text):
             return c
     return False
 
+def get_cur_exchange_rate(cur1, cur2):
+    r = requests.get('https://duckduckgo.com/js/spice/currency/1/{}/{}'.format(cur1, cur2))
+
+    if (r.status_code != 200):
+        return False
+
+    try:
+        unwrapped_response = r.text[r.text.find('\n') + 1 : r.text.rfind('\n') - 2]
+        json_response = json.loads(unwrapped_response)
+        value = json_response['to'][0]['mid']
+    except:
+        return False
+    return value
+
+async def shit_broke(message):
+    await message.reply("Shit broke. You're either brainded or blame [DuckDuckGo](https://duckduckgo.com).")
+    return True
+
 class MyClient(discord.Client):
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
@@ -49,77 +68,62 @@ class MyClient(discord.Client):
             command = message.content.split()[0][1:]
             if command == "convert":
                 args = message.content.split()[1:]
-                yesterday = datetime.strftime(datetime.now() - timedelta(1), '%Y-%m-%d')
-                today = datetime.strftime(datetime.now(), '%Y-%m-%d')
                 if len(args) == 3:
                     amount = args[0]
                     currency = args[1]
                     to_currency = args[2]
-                    converted = api.get_exchange_rates(
-                        base_currency=currency,
-                        start_date=yesterday,
-                        end_date=today,
-                        targets=[currency, to_currency]
+                    converted = get_cur_exchange_rate(
+                        currency,
+                        to_currency
                     )
-                    converted = converted[yesterday][to_currency.upper()] * float(amount)
-                    converted = converted.__round__(2)
-                    await message.reply(f'{amount} {currency.upper()} is ~{converted} {to_currency.upper()}.')
+                    if (not converted):
+                        await shit_broke(message)
+                        return
+                    print(amount)
+                    print(converted)
+                    total_value = float(amount) * float(converted)
+                    await message.reply(f'{amount} {currency.upper()} is ~{round(total_value, 3)} {to_currency.upper()}.')
                 else:
                     await message.reply('Invalid arguments')
             return
 
         matches = re.finditer(CURRENCYREGEX, message.content, re.MULTILINE)
-        if not matches:
-            print()
-            return
-        
-        yesterday = datetime.strftime(datetime.now() - timedelta(1), '%Y-%m-%d')
-        today = datetime.strftime(datetime.now(), '%Y-%m-%d')
+        print('')
 
         currency_data = []
+
         for matchNum, match in enumerate(matches, start=1):
             amount_unwrapped = float(match.group(1))
             amount_k = len(match.group(2)) if match.group(2) else 0
             currency = find_currency(match.group(3))
-            if currency == None:
-                currency = find_currency(match.group(2) + match.group(3))
-                if currency == None:
-                    continue
-                else:
-                    amount_k = 0
-            currency = currency['cc'].upper()
-            amount = amount_unwrapped * (1000 ** amount_k)
             
-            rates = api.get_exchange_rates(
-                base_currency=currency,
-                start_date=yesterday,
-                end_date=today,
-                targets=ENVRATE
-            )
+            if (amount_k > 0):
+                amount_unwrapped = amount_unwrapped * (1000 * amount_k)
             
-            matched_rates = {}
-            for rate in rates[yesterday]:
-                if rate != currency:
-                    matched_rates[rate] = (rates[yesterday][rate] * amount).__round__(2)
+            exchange_rate = get_cur_exchange_rate(currency['cc'], ENVRATE[0])
+            if (not exchange_rate):
+                await shit_broke(message)
+                return
+            total_value = amount_unwrapped * exchange_rate
 
-            currency_data.append({
-                "amount": f'{amount:,}',
-                "currency": currency,
-                "rates": matched_rates
-            })
-
-            print(f" ({amount} {currency})", end='')
-
-        if len(currency_data) == 0:
-            return
-
-        message_to_send = f"### <a:DinkDonk:956632861899886702> {len(currency_data)} money mention{'s' if len(currency_data) > 1 else ''} detected\n\n" + \
-            '\n'.join([f"* **{data['amount']} {data['currency']}** is {' or '.join([f'*{value:,} {key}*' for key, value in data['rates'].items()])}" for data in currency_data])
+            currency_data.append('{} **{}** is {} **{}**'.format(
+                amount_unwrapped,
+                currency['cc'].upper(),
+                round(total_value, 3),
+                find_currency(ENVRATE[0])['cc'].upper()
+            ))
         
-        await message.reply(message_to_send)
 
-      
-        print('')
+        if (len(currency_data) < 1):
+            return
+        
+        response_text = "### <a:DinkDonk:956632861899886702> {} currency mentions found.\n".format(len(currency_data))
+
+        for k, v in enumerate(currency_data):
+            response_text += '{}. {}'.format(k+1, v)
+        
+        await message.reply(response_text)
+
             
 intents = discord.Intents.default()
 intents.message_content = True
