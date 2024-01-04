f6c80242899279926eabbfe9ffd621ed3f6bb317
Mikhail <me@reze.moe>
2024-01-04 12:32:58 +0900

    Make convert command work with aliases

--- bot.py
+++ bot.py
@@ -72,19 +72,23 @@ class MyClient(discord.Client):
                 args = message.content.split()[1:]
                 if len(args) == 3:
                     amount = args[0]
-                    currency = args[1]
-                    to_currency = args[2]
+                    currency = find_currency(args[1])
+                    to_currency = find_currency(args[2])
+
+                    if currency == None or to_currency == None:
+                        await message.reply("Please check the currency specified.")
+                        return
+
                     converted = get_cur_exchange_rate(
-                        currency,
-                        to_currency
+                        currency['cc'],
+                        to_currency['cc']
                     )
                     if (not converted):
                         await shit_broke(message)
                         return
-                    print(amount)
-                    print(converted)
+
                     total_value = float(amount) * float(converted)
-                    await message.reply(f'{amount} {currency.upper()} is ~{round(total_value, 3)} {to_currency.upper()}.')
+                    await message.reply(f'{amount} {currency["cc"].upper()} is ~{round(total_value, 3)} {to_currency["cc"].upper()}.')
                 else:
                     await message.reply('Invalid arguments')
             return
