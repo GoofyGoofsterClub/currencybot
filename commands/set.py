a9db7770eeea5173c21a6bc768adf1252969a6e6
LMNYX <me@reze.moe>
2025-07-05 23:56:19 +0500

    feat: allow unsetting user preference variables through the use of -reset argument

--- commands/set.py
+++ commands/set.py
@@ -18,18 +18,30 @@ async def _set(message, args, _globals):
         return
     if len(args) == 2:
         var = args[0].lower()
-        if var in SUPPORTED_VARIABLES and validate_type(args[1], SUPPORTED_VARIABLES[var]):
-            try:
-                await asyncio.to_thread(_globals['currdb']['user_preferences'].update_one,
-                    {"user_id": message.author.id},
-                    {"$set": {var: args[1]}},
-                    upsert=True
-                )
-                await message.reply(f"Set `{var}` for {message.author.mention} to `{args[1]}`")
-            except Exception as e:
-                await message.reply(f"An error occurred while processing this request. ({e})")
+        if var in SUPPORTED_VARIABLES:
+            if args[1].lower() == "-reset":
+                try:
+                    await asyncio.to_thread(_globals['currdb']['user_preferences'].update_one,
+                        {"user_id": message.author.id},
+                        {"$unset": {var: ""}},
+                        upsert=True
+                    )
+                    await message.reply(f"Removed `{var}` for {message.author.mention}")
+                except Exception as e:
+                    await message.reply(f"An error occurred while processing this request. ({e})")
+            elif validate_type(args[1], SUPPORTED_VARIABLES[var]):
+                try:
+                    await asyncio.to_thread(_globals['currdb']['user_preferences'].update_one,
+                        {"user_id": message.author.id},
+                        {"$set": {var: args[1]}},
+                        upsert=True
+                    )
+                    await message.reply(f"Set `{var}` for {message.author.mention} to `{args[1]}`")
+                except Exception as e:
+                    await message.reply(f"An error occurred while processing this request. ({e})")
+            else:
+                await message.reply(f"Invalid value for `{var}`. Supported values: {', '.join([str(t) for t in SUPPORTED_VARIABLES[var].__args__])}")
         else:
             await message.reply(f"Invalid variable. Supported variables: {', '.join(SUPPORTED_VARIABLES.keys())}")
     else:
         await message.reply('Invalid arguments. Example: $set <variable> <value>')
-    pass
